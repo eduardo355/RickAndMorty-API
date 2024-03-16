baff739da320eaf7c2163e0367c03f98919eb825
couch355 <tovareduardo75@gmail.com>
2024-03-15 22:11:41 -0600

    New name to page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,9 +2,9 @@ import RickAndMorty from "../Icons/RickAndMory"
 
 const Nav = () => {
     return (
-        <nav className="flex items-center max-sm:justify-center gap-[1em] p-[1em]">
+        <nav className="flex items-center max-sm:justify-center gap-[1em] p-[.5em] bg-slate-900 mb-4 shadow-md">
             <RickAndMorty />
-            <h1 className="text-2xl font-bold">Rick And Morty - API</h1>
+            <h1 className="text-2xl font-bold text-white">Rick And Morty</h1>
         </nav>
     )
 }
